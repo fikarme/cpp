7982acf9d40d030d0cf30de2c20e5557ec87e937
fikarme <fikarme@pm.me>
2025-01-05 16:59:12 +0300

    3ü geri koydum yerine

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -1,7 +1,7 @@
 #pragma once
-#include  <iostream>
+#include <iostream>
 
-using  std::string;
+using std::string;
 
 class Bureaucrat
 {
@@ -29,3 +29,28 @@ public:
 	};
 };
 
+#ifndef BUREAUCRAT_HPP
+#define Bureaucrat_HPP
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class Animal{
+    protected:
+        string _type;
+    public:
+        virtual ~Animal();
+        Animal();
+        Animal(string type);
+        Animal(const Animal &copy);
+        Animal &operator=(const Animal &copy);
+        string getType() const;
+        virtual void makeSound() const;
+		
+};
+
+#endif
